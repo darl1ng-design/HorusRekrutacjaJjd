5945608a19b1e1e1e14965039bfd48cae8d5a8a2
darl1ng-design <adam.sp220@gmail.com>
2025-10-15 20:32:29 +0200

    changes

--- src/main/java/Cabinet.java
+++ src/main/java/Cabinet.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.Optional;
+
 public interface Cabinet {
     Optional<Folder> findFolderByName(String name);
     List<Folder> findFoldersBySize(String size); // Small, medium, large
