4d81e141be709dbd2095d63617acf7c2f5e07f76
darl1ng-design <adam.sp220@gmail.com>
2025-10-16 13:14:14 +0200

    Committing final implementation

--- src/main/java/FolderImpl.java
+++ src/main/java/FolderImpl.java
@@ -1,27 +1,4 @@
-public class FolderImpl implements Folder{
-    private String name;
-    private String size;
+package PACKAGE_NAME;
 
-    public FolderImpl(String name, String size) {
-        this.name = name;
-        this.size = size;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public String getSize() {
-        return size;
-    }
-
-    @Override
-    public String toString() {
-        return "FolderImpl{" +
-                "name='" + name + '\'' +
-                ", size='" + size + '\'' +
-                '}';
-    }
+public class FolderImpl {
 }
