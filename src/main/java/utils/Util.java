658a47a3d928708ed3320615ace916d422240049
darl1ng-design <adam.sp220@gmail.com>
2025-10-18 09:34:05 +0200

    added javadoc to the Util class

--- src/main/java/utils/Util.java
+++ src/main/java/utils/Util.java
@@ -1,3 +1,20 @@
+/**
+ * Utility class providing normalization and validation methods
+ * for string input values used across the application.
+ * <p>
+ * This class is stateless and cannot be instantiated.
+ * All methods are {@code static} and designed for safe,
+ * lightweight preprocessing of user-supplied or
+ * domain-related strings.
+ * </p>
+ *
+ * <h2>Responsibilities</h2>
+ * <ul>
+ *     <li>Normalize string input by trimming and lowercasing.</li>
+ *     <li>Validate and enforce allowed folder size categories.</li>
+ * </ul>
+ */
+
 package utils;
 
 
