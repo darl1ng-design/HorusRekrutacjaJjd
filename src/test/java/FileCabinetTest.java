b0128bc1a6ca56a63ad28c5ac34983e87348b4df
darl1ng-design <adam.sp220@gmail.com>
2025-10-18 11:29:29 +0200

    added two new test methdods: testCountWhenFolderIsEmpty and testCountWhenFoldersIsNotEmpty

--- src/test/java/FileCabinetTest.java
+++ src/test/java/FileCabinetTest.java
@@ -19,6 +19,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -97,5 +99,14 @@ public class FileCabinetTest {
         Assertions.assertEquals(0, fileCabinet.count());
     }
 
+    @Test
+    void testCountWhenFoldersIsEmpty(){
+        fileCabinet.setFolders(new ArrayList<>());
+        Assertions.assertEquals(0, fileCabinet.count());
+    }
 
+    @Test
+    void testCountWhenFoldersIsNotEmpty(){
+        Assertions.assertEquals(fileCabinet.getFolders().size(), fileCabinet.count());
+    }
 }
