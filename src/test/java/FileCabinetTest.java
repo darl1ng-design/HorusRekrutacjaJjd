d998ef2f1e170007cfebefb8b7f45f6a0479fa33
darl1ng-design <adam.sp220@gmail.com>
2025-10-18 08:00:23 +0200

    Implemented initial test method

--- src/test/java/FileCabinetTest.java
+++ src/test/java/FileCabinetTest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+
+
+@DisplayName("Test container for FileCabinet class")
 public class FileCabinetTest {
+
+    private FileCabinet fileCabinet;
+
+
+    @Test
+    void testFinderFolderByName(){
+        Assertions.assertNull(fileCabinet.findFolderByName("test1"));
+    }
 }
